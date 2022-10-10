8561535419868ca354a724c5b6170f4bf35703bc
Peter <peter-lauridsen@live.dk>
2022-10-10 14:09:31 +0200

    second commit

---
+++ src/comp/advSection.js
@@ -0,0 +1,27 @@
+import { advantages } from "../data.js"
+import advArticle from "./advArticle.js"
+import "./advSection.scss"
+
+
+console.log(advantages)
+let advSection = function() {
+
+let element = document.createElement('div')
+element.classList.add('advantages')
+
+advantages.forEach(advantage => {
+    console.log(advantage)
+
+    let article = document.createElement("article")
+
+    element.append(article)
+    element.append(advArticle(advantage))
+
+
+})
+
+
+return element
+}
+
+export default advSection

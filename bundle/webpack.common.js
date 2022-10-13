43f1e7f70f5f39950d4294fd8993acb1e99e6567
Peter <peter-lauridsen@live.dk>
2022-10-13 09:35:29 +0200

    added new webpacks

--- bundle/webpack.common.js
+++ bundle/webpack.common.js
@@ -1,6 +1,6 @@
 const path = require("path");
-const miniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: "./src/index.js",
@@ -8,32 +8,23 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "build")
     },
-    devtool: "inline-source-map",
-    devServer: {
-        open: true,
-        static: {
-            directory: path.join(__dirname, "build")
-        },
-        port: 3000,
-    },
-        plugins:[
-            new HtmlWebpackPlugin({
-                template: 'template.html'
-            }),
-            new miniCssExtractPlugin({
-                linkType:"text/css"
-            })
-        ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'template.html'
+        }),
+        new MiniCssExtractPlugin({
+            linkType: "text/css"
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.(css|sass|scss)$/i,
-                use: [miniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /\.(png|jpg|jpeg|svg|webp|gif|heic)$/i,
                 type: "asset/resource"
-
             },
             {
                 test: /\.js$/i,
